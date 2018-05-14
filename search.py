4310c11dcf2bcfafd9e360af3419fdab6da18971
Ashit Mundra <amundra6@gmail.com>
2018-05-14 14:34:01 +0530

    ll

---
+++ search.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+a= raw_input(), raw_input() , raw_input(), raw_input(), raw_input()
+for i in a:
+	if "ll" in str(i):
+		print  "True",  "    ", a.index(i), "    ", i
+	else:
+		print  "False", "    ", i
+
+
+
